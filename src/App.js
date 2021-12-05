785c917d7a80e648358be13d0220f5afe9e34470
Gonzalo pintado <pintadogonzalo2@gmail.com>
2021-12-04 21:47:12 -0300

    entrega Rutas

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
-          <Route path='/detail/:id' element={<ItemDetailContainer />} />
+          <Route path='/item/:id' element={<ItemDetailContainer />} />
           
         </Routes>
       </>
