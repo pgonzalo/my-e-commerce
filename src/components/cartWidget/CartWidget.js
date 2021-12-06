5a0d78cdf6db8a29b5b6cf2b28845c26223f4eb2
Gonzalo pintado <pintadogonzalo2@gmail.com>
2021-12-06 18:04:34 -0300

    rutas

--- src/components/cartWidget/CartWidget.js
+++ src/components/cartWidget/CartWidget.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BsCart3 } from 'react-icons/bs';
-// import { Button } from 'semantic-ui-react';
-// import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import './CartWidget.css';
  
 function CartWidget() {
     return (
         <div>
-                  {/* <Link to={`/cart`}>
-        <Button id="button" variant="green">Agregar</Button>
-      </Link> */}
-            <BsCart3 className="cart-icon" />
+                  { <Link to={`/cart`}>
+        <Button id="button" variant="green"><BsCart3 className="cart-icon" /></Button>
+      </Link> }
+           {/* <BsCart3 className="cart-icon" /> */}
         </div>
     )
 }
